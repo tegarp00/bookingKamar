c60c32bc2034f9dc664f2c08ec669f3f01590abf
t394rp00 <tegarp00@gmail.com>
2022-05-12 15:44:13 +0000

    baru selesai create DB hotels

--- index.js
+++ index.js
@@ -24,6 +24,9 @@ mongoose.connection.on("disconnect", ()=> {
 });
 
 // Middlewares
+
+app.use(express.json())
+
 app.use("/api/auth", authRoute)
 app.use("/api/users", usersRoute)
 app.use("/api/hotels", hotelsRoute)
--- routes/hotels.js
+++ routes/hotels.js
@@ -1,6 +1,23 @@
 import express from "express"
+import Hotel from "../models/Hotel.js"
 
 const router = express.Router()
 
+//CREATE
+router.post("/", async (req, res) => {
+  const newHotel = new Hotel(req.body)
+
+  try {
+    const savedHotel = await newHotel.save()
+    res.status(200).json(savedHotel)
+  }catch(err){
+    res.status(500).json(err);
+  }
+})
+
+//UPADTE
+//DELETE
+//GET
+//GET ALL
 
 export default router
