c304a8b53306f586fe576f8819bae6efab7bb1a5
t394rp00 <tegarp00@gmail.com>
2022-05-15 16:12:46 +0000

    baru siap menambahkan controller hotel

--- index.js
+++ index.js
@@ -24,13 +24,25 @@ mongoose.connection.on("disconnect", ()=> {
 });
 
 // Middlewares
-
 app.use(express.json())
 
 app.use("/api/auth", authRoute)
 app.use("/api/users", usersRoute)
 app.use("/api/hotels", hotelsRoute)
 app.use("/api/rooms", roomsRoute)
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500
+  const errorMessage = err.message || "Something went wrong"
+  const custStack = err.stack || "oalah"
+
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack
+  })
+})
 
 app.listen(8800, () => {
   connect()
--- routes/hotels.js
+++ routes/hotels.js
@@ -1,23 +1,19 @@
 import express from "express"
 import Hotel from "../models/Hotel.js"
+import createError from "../utils/error.js"
+import { createHotel, updateHotel, deleteHotel, getHotel, getHotels } from "../controllers/hotel.js"
 
 const router = express.Router()
 
 //CREATE
-router.post("/", async (req, res) => {
-  const newHotel = new Hotel(req.body)
-
-  try {
-    const savedHotel = await newHotel.save()
-    res.status(200).json(savedHotel)
-  }catch(err){
-    res.status(500).json(err);
-  }
-})
-
+router.post("/", createHotel)
 //UPADTE
+router.put("/:id", updateHotel)
 //DELETE
+router.delete("/:id", deleteHotel)
 //GET
+router.get("/:id", getHotel)
 //GET ALL
+router.get("/", getHotels)
 
 export default router
