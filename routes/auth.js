bf68ed1da583dacba75501b516236dcb844204ff
t394rp00 <tegarp00@gmail.com>
2022-06-26 07:35:49 +0000

    api-bookingHotels

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,10 @@
 import express from "express"
+import {register, login} from "../controllers/auth.js"
 
 const router = express.Router()
 
-router.get("/", (req, res) => {
-  res.send("Hello, this is auth endpoint")
-})
+router.post("/register", register)
+router.post("/login", login)
 
-router.get("/register", (req, res) => {
-  res.send("Hello, this is register endpoint")
-})
 
 export default router
